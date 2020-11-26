03046a36342e9f1fa1615f2eed052fdd2635895f
hrealinho <h.realinho@campus.fct.unl.pt>
2020-11-26 20:40:25 +0000

    Update Homepage.js

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,17 +4,37 @@ import {BiMapAlt} from "react-icons/bi";
 import {AiOutlineHome,AiOutlineQuestionCircle,AiOutlineFieldTime} from "react-icons/ai";
 import './Homepage.css'
 import {withRouter} from "react-router-dom";
-import {BiMapAlt} from "react-icons/bi"
 import Footer from './Footer'
+
+var departure = "";
+var arrival = "";
+
 class Homepage extends Component {
 
     constructor() {
         super();
     }
 
-    changePage(page) {
-        this.state.page = page
-        history.push(page)
+    switch(mode) {
+      if (mode.equals("l")) {
+        n = departure;
+        departure = arrival;
+        arrival = n;
+      }
+      else {
+        n = arrival;;
+        arrival = departure;
+        departure = n;
+      }
+    }
+
+    insertDeparture(input) {
+      departure = input;
+    }
+
+
+    insertArrival(input) {
+      arrival = input;
     }
 
     render() { // arrows TODO, logos TODO
@@ -25,17 +45,17 @@ class Homepage extends Component {
             <form className = "DepartureBox">
               <label>
                 <p className = "BoxText"> Departure </p>
-                <input type="text" name="departure" />
+                <input type="text" name="departure" id="input" name="input" onInput="insertDeparture('input');"/>
               </label>
             </form>
 
             <div className = "ArrowLeft">
-              <button className = "SwitchButton" onClick={() => history.push("/ipm_project/find/")}>
+              <button className = "SwitchButton" onClick={() => switch("l"); }>
                 <img src={process.env.PUBLIC_URL + '/imgs/arrow_left.png'} style={{opacity: 1,transform:"scale(0.05)"}} className="ArrowLeft" />
               </button>
             </div>
             <div className = "ArrowRight">
-              <button className = "SwitchButton" onClick={() => history.push("/ipm_project/find/")}>
+              <button className = "SwitchButton" onClick={() => switch("r")}>
                 <img src={process.env.PUBLIC_URL + '/imgs/arrow_right.png'} style={{opacity: 1,transform:"scale(0.05)"}} className="ArrowLeft" />
               </button>
             </div>
@@ -43,11 +63,11 @@ class Homepage extends Component {
             <form className = "ArrivalBox">
               <label>
                 <p className = "BoxText"> Arrival </p>
-                <input type="text" name="arrival" />
+                <input type="text" name="arrival" id="input" name="input" onInput="insertArrival('input');"/>
               </label>
             </form>
 
-            <button className = "FindButton" onClick={() => history.push("/ipm_project/find/")}>Find</button>
+            <button className = "FindButton" onClick={() => history.push("/ipm_project/find//{{departure}}/{{arrival}}")}>Find</button>
           </div>
 
         </div>
