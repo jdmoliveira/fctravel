657f60707dc7c55f4dfe5e9e46fa4ae82da28c0c
hrealinho <h.realinho@campus.fct.unl.pt>
2020-11-28 19:19:55 +0000

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -22,7 +22,7 @@ class Footer extends Component {
   render() {
     return(
     <div className = "Footer" >
-      <img src={bus_logo} style={{"transform":"scale(1.5)", "marginLeft":"11vh", "marginTop":"65vh", "float":"left"}} className="Bus-logo" alt="bus_logo" />
+      <img src={bus_logo} style={{"transform":"scale(1.5)", "marginLeft":"11vh", "marginTop":"68vh", "float":"left"}} className="Bus-logo" alt="bus_logo" />
       <img src={boat_logo} style={{"transform":"scale(2)", "marginLeft":"45vh","marginTop":"65vh","float":"middle"}} className="Boat-logo" alt="boat_logo" />
       <img src={train_logo} style={{"transform":"scale(1.5)", "marginRight":"13vh","marginTop":"65vh", "float":"right"}} className="Train-logo" alt="train_logo" />
     </div>
