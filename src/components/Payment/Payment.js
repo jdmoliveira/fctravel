5e0964383f26ff4433e07fed4c5ed5807dde713b
José Oliveira <j_diogoliveira@hotmail.com>
2020-11-30 10:58:54 +0000

    Payment done

--- src/components/Payment/Payment.js
+++ src/components/Payment/Payment.js
@@ -19,7 +19,7 @@ class Payment extends Component {
 
     constructor(props) {
         super(props);
-        console.log(this.props)
+
     }
 
 
@@ -40,43 +40,45 @@ class Payment extends Component {
             <div className ="PaymentPage">
 
                 <h3 style ={{"fontWeight":"bold", "textAlign":"center","textDecoration":"underline"}}>Ticket Information</h3>
-                <div style={{"position":"absolute"}}>
+                <div className="Objects">
+                <div style={{"position":"relative"}}>
                     <>
                         {this.props.location.state.route === "t1r1"?
-                            <img src = {t1r1} style={{"transform":"scale(0.8)"}} alt="t1r1"></img>
+                            <img src = {t1r1} style={{"position":"absolute","transform":"scale(0.8)","marginTop":"-23vh"}} alt="t1r1"></img>
                         : this.props.location.state.route === "t1r2"?
-                                <img src = {t1r2} style={{"transform":"scale(0.8)"}} alt="t1r2"></img>
+                                <img src = {t1r2} style={{"position":"absolute","transform":"scale(0.8)","marginTop":"-23vh"}} alt="t1r2"></img>
                         : this.props.location.state.route === "t2r1" ?
-                                    <img src = {t2r1} style={{"transform":"scale(0.8)"}} alt="t2r1"></img>
+                                    <img src = {t2r1} style={{"position":"absolute","transform":"scale(0.8)","marginTop":"-23vh"}} alt="t2r1"></img>
                                     :this.props.location.state.route === "t2r2"?
-                                        <img src = {t2r2} style={{"transform":"scale(0.8)"}} alt="t2r2"></img>
+                                        <img src = {t2r2} style={{"position":"absolute","transform":"scale(0.8)","marginTop":"-23vh"}} alt="t2r2"></img>
                                     :null
                         }
                     </>
                 </div>
-                <div style={{"position":"absolute","marginLeft":"70vw","marginTop":"20vh"}}>
-                    <span style={{"fontSize":"large","fontWeight":"bold"}}>Price: </span>
-                    <span>{this.props.location.state.price}</span>
+                <div style={{"position":"relative","marginLeft":"70vw","marginTop":"20vh"}}>
+                    <span style={{"position":"absolute","fontSize":"large","fontWeight":"bold"}}>Price: </span>
+                    <span style={{"position":"absolute","marginLeft":"3.5vw"}}>{this.props.location.state.price}</span>
                 </div>
-                <div style={{"position":"absolute","marginLeft":"70vw","marginTop":"26vh"}}>
-                    <span style={{"fontSize":"large","fontWeight":"bold"}}>Payment method:</span>
+                <div style={{"position":"relative","marginLeft":"70vw","marginTop":"26vh"}}>
+                    <span style={{"position":"absolute","fontSize":"large","fontWeight":"bold","marginTop":"10vh"}}>Payment method:</span>
                     <>
                         {this.props.location.state.method === "mb" ?
-                            <img src = {mb} style={{"transform" : "scale(0.3)","marginLeft":"8vw","marginTop":"-19vh"}}></img>
+                            <img src = {mb} style={{"position":"absolute","transform" : "scale(0.3)","marginLeft":"8vw","marginTop":"-5vh"}}></img>
                         :this.props.location.state.method === "mastercard"?
-                                <img src = {mastercard} style={{"marginTop":"-70vh","marginLeft":"-24vw","transform" : "scale(0.1)"}}></img>
+                                <img src = {mastercard} style={{"position":"absolute","marginTop":"-57vh","marginLeft":"-24vw","transform" : "scale(0.1)"}}></img>
                         :this.props.location.state.method === "visa"?
-                                    <img src = {visa} style={{"marginTop":"-60vh","marginLeft":"-25vw","transform" : "scale(0.09)"}}></img>
+                                    <img src = {visa} style={{"position":"absolute","marginTop":"-45vh","marginLeft":"-25vw","transform" : "scale(0.09)"}}></img>
                         :this.props.location.state.method === "paypal"?
-                                        <img src = {paypal} style={{"marginTop":"-35vh","marginLeft":"-33vw","transform" : "scale(0.09)"}}></img>
+                                        <img src = {paypal} style={{"position":"absolute","marginTop":"-20vh","marginLeft":"-33vw","transform" : "scale(0.09)"}}></img>
                         :null}
                     </>
                 </div>
                 <div style={{"position":"relative","float":"right"}}>
-                    <h5 style ={{"fontWeight":"bold", "textAlign":"center","marginLeft":"-30vw","marginTop":"50vh"}}>Are you sure you want to buy this ticket? </h5>
+                    <h5 style ={{"fontWeight":"bold", "textAlign":"center","marginLeft":"-30vw","marginTop":"23vh"}}>Are you sure you want to buy this ticket? </h5>
                     <input className ="paymentButtons" type="image" src={no}  style={{"marginTop":"-27vh","marginLeft":"-32vw"}}onClick={()=>this.dontBuyTicket()}alt ="no"/>
                     <input className ="paymentButtons"type="image" src={yes} style={{"marginTop":"-27vh","marginLeft":"-16vw"}} onClick={()=>this.buyTicket()} alt = "yes"/>
 
+                </div>
                 </div>
 
 
--- src/components/PaymentMethod/PaymentMethod.js
+++ src/components/PaymentMethod/PaymentMethod.js
@@ -48,7 +48,7 @@ class PaymentMethod extends Component{
     }
     changeShow(){
         this.props.onChange()
-        history.push("/ipm_project/payment")
+        history.push({pathname:"/ipm_project/find/", state:{departure:this.props.departure}})
     }
 
 
