12e1a8edfd561ea08720e6dbc1a1c1af8701b52f
José Oliveira <j_diogoliveira@hotmail.com>
2020-11-30 16:14:35 +0000

    qdasf

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -22,7 +22,7 @@ class Footer extends Component {
       <div className = "Footer" >
         <img src={bus_logo} style={{"transform":"scale(1)", "marginLeft":"11vh", "marginTop":"66vh", "float":"left"}} className="Bus-logo" alt="bus_logo" />
         <img src={boat_logo} style={{"transform":"scale(1.3)scaleX(1.2)", "marginLeft":"45vh","marginTop":"65vh","float":"middle"}} className="Boat-logo" alt="boat_logo" />
-        <img src={train_logo} style={{"transform":"scale(0.9)scaleX(1.45)", "marginRight":"13vh","marginTop":"65vh", "float":"right"}} className="Train-logo" alt="train_logo" />
+        <img src={train_logo} style={{"transform":"scale(0.9)scaleX(-1.35)", "marginRight":"13vh","marginTop":"65vh", "float":"right"}} className="Train-logo" alt="train_logo" />
       </div>
     )
   }
